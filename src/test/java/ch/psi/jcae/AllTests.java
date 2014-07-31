8e7806e1a9da79bd766c1f532c9779210e5ab8e7
maerki_f <fabian.maerki@psi.ch>
2014-07-31 09:04:14 +0200

    Add test

--- src/test/java/ch/psi/jcae/AllTests.java
+++ src/test/java/ch/psi/jcae/AllTests.java
@@ -4,12 +4,15 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import ch.psi.jcae.cas.ProcessVariableTest;
+
 @RunWith(Suite.class)
 @SuiteClasses( { 	ChannelServiceAnnotatedObjectTest.class, 
 					ChannelServiceTest.class, 
 					ChannelTest.class, 
 					JCAChannelFactoryTest.class,
-					JcaePropertiesTest.class })
+					JcaePropertiesTest.class,
+					ProcessVariableTest.class })
 
 public class AllTests {
 }
