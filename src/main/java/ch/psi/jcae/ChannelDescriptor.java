5e95121ade0cf819d1627bd71ff4b0957739291a
ebner <simon.ebner@psi.ch>
2015-04-29 09:09:35 +0200

    added convenience constructor for matlab usage

--- src/main/java/ch/psi/jcae/ChannelDescriptor.java
+++ src/main/java/ch/psi/jcae/ChannelDescriptor.java
@@ -25,10 +25,69 @@ public class ChannelDescriptor<T> extends Descriptor<T> {
 		this.size = size;
 	}
 
+	// Convenience constructors
+	
+	public ChannelDescriptor(String type, String name){
+		this.name = name;
+		this.type = getDataType(type);
+	}
+	
+	public ChannelDescriptor(String type, String name, Boolean monitored){
+		this.name = name;
+		this.type = getDataType(type);
+		this.monitored = monitored;
+	}
+	
+	public ChannelDescriptor(String type, String name, Boolean monitored, Integer size){
+		this.name = name;
+		this.type = getDataType(type);
+		this.monitored = monitored;
+		this.size = size;
+	}
+	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private Class<T> getDataType(String type){
+		type = type.toLowerCase();
+		switch (type) {
+		case "double":
+			return (Class<T>) Double.class;
+		case "integer":
+			return (Class<T>) Integer.class;
+		case "short":
+			return (Class<T>) Short.class;
+		case "float":
+			return (Class<T>) Float.class;
+		case "byte":
+			return (Class<T>) Byte.class;
+		case "boolean":
+			return (Class<T>) Boolean.class;
+		case "string":
+			return (Class<T>) String.class;
+
+		case "double[]":
+			return (Class<T>) double[].class;
+		case "integer[]":
+			return (Class<T>) int[].class;
+		case "short[]":
+			return (Class<T>) short[].class;
+		case "float[]":
+			return (Class<T>) float[].class;
+		case "byte[]":
+			return (Class<T>) byte[].class;
+		case "boolean[]":
+			return (Class<T>) boolean[].class;
+		case "string[]":
+			return (Class<T>) String[].class;
+			
+		default:
+			throw new IllegalArgumentException("Type "+type+" is not supported");
+		}
 	}
 }
